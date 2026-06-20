da495025ba480e8a756a397f09bae7b13e18af01
KaueSiqueira <kaue54150@gmail.com>
2026-06-20 18:21:40 -0300

    Tela de ajuda iniciada

--- src/renderer/src/pages/Ajuda.jsx
+++ src/renderer/src/pages/Ajuda.jsx
@@ -1,7 +1,14 @@
 import styles from '../css/ajuda.module.css'
 import Logo from '../assets/logo.png'
+import { useNavigate } from 'react-router-dom'
 
 function Ajuda() {
+  const navigate = useNavigate()
+
+  function Sobre() {
+    navigate('../pages/Ajuda')
+  }
+
   return (
     <section className={styles.section}>
       <div className={styles.logo}>
@@ -11,10 +18,36 @@ function Ajuda() {
       <h1>Ajuda</h1>
 
       <div className={styles.container}>
-        <div className={styles.links}>
-          <a href="">Sobre</a>
+        <div className={styles.links_container}>
+          <div className={styles.links}>
+            <a onClick={() => Sobre}>Sobre</a>
+            <a href="">Como funciona - pessoa surda</a>
+            <a href="">Como funciona - pessoa ouvinte</a>
+          </div>
+          <button onClick={''} className={styles.btn}>
+            Voltar
+          </button>
         </div>
-        <div className={styles.content}></div>
+        <div className={styles.content_container}>
+          <div className={styles.content}>
+            <p>
+              O <span>Sinaliza</span>
+              <span className={styles.ai}>AI</span> nasceu para transformar acessibilidade em algo{' '}
+              <span>natural, humano e presente no cotidiano.</span>
+            </p>
+            <p>
+              Unindo <span>inteligência artificial, inclusão e inovação</span>, criamos uma nova
+              forma de aproximar pessoas <span>surdas</span> e <span>ouvintes</span> em qualquer
+              ambiente.
+            </p>
+            <p>
+              Desenvolvemos experiências capazes de <span>reduzir barreiras de comunicação</span> e
+              <span>promover autonomia através da inovação.</span> Acreditamos que{' '}
+              <span>acessibilidade</span>
+              não deve ser adaptação. Deve ser padrão.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   )
